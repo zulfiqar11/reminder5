1c0938d366263dbb0f3515896eb025735cd4b521
zulfiqarsyed <zulfiqarsyed@yahoo.com>
2022-02-24 02:44:27 -0800

    minor updates to navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -41,9 +41,16 @@ export class AppComponent implements OnInit {
   // todo: only for chrome when the f12 network tab is open, the routing does not seem to work.
   // tried for over an hour.. chrome does not support this.
 
+  // todo: navigation its own component instead of app component?
+
+  // todo: add login screen via firebase and google login.
+  // todo: manage logout button with login and rest of buttons based on login screen.
+  // todo: manage other buttons based on login
+
+  // todo: add jwt login
+
   // tood: add hard coded reminders with material table.
   // todo: add material table for contacts
-  // todo: add login screen via reactive form
-  // todo: add jwt login
   // todo: add httpinterceptor for jwt login
   // todo: add auth guard for reminders and contacts screen
+
