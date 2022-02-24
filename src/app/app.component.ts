2511a1b8a3c06e18e6c06d146295788334e2423a
zulfiqarsyed <zulfiqarsyed@yahoo.com>
2022-02-23 19:58:50 -0800

    added top and side navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { BreakpointObserver } from '@angular/cdk/layout';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,21 +9,41 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'reminder 5';
 
+  @ViewChild(MatSidenav) sidenav!: MatSidenav;
+
+  constructor(private observer: BreakpointObserver) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    //Called after ngAfterContentInit when the component's view has been initialized. Applies to components only.
+    //Add 'implements AfterViewInit' to the class.
+
+    this.observer.observe(['(max-width:800px)'])
+      .subscribe((res) => {
+        if (res.matches) {
+          this.sidenav.mode = 'over';
+          this.sidenav.close();
+        } else {
+          this.sidenav.mode = 'side';
+          this.sidenav.open();
+        }
+      } )
+
+  }
+}
+
+
   // todo: only for chrome when the f12 network tab is open, the routing does not seem to work.
   // tried for over an hour.. chrome does not support this.
 
-  // todo: add this project to github
-  // todo: add top material navigation
-  // todo: add side material navigation
   // tood: add hard coded reminders with material table.
   // todo: add material table for contacts
   // todo: add login screen via reactive form
   // todo: add jwt login
   // todo: add httpinterceptor for jwt login
   // todo: add auth guard for reminders and contacts screen
-
-
-  ngOnInit(): void {
-
-  }
-}
--- src/app/share/material.module.ts
+++ src/app/share/material.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
 
 
 @NgModule({
   declarations: [],
   exports: [
-    MatButtonModule
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    MatSidenavModule
   ],
   imports: [
     CommonModule,
